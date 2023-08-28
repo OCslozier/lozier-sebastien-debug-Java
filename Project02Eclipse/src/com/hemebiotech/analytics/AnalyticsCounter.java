0beee109e1058e3fb2162eb5891f76df80949ec1
Sebastien Lozier <sebastien.lozier@gmail.com>
2023-08-28 15:08:48 +0200

    Unnecessary comment removed

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,8 +5,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
+	private static int headacheCount = 0;	
+	private static int rashCount = 0;		
 	private static int dialatedpupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
