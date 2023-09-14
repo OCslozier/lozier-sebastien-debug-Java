c62b92ae5c4fd720071d5867cd9b4b55df428f6e
Sebastien Lozier <sebastien.lozier@gmail.com>
2023-09-14 15:26:39 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,32 +1,63 @@
 package com.hemebiotech.analytics;
 
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.List;
 
-
 public class AnalyticsCounter {
-	
+
+	ISymptomReader reader;
+	ISymptomWriter writer;
+
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		this.reader = reader;
+		this.writer = writer;
+
+	}
+
+	public List<String> getSymptoms() {
+
+		return reader.GetSymptoms();
+
+	}
+
+	public Map<String, Integer> countOccurrences(List<String> symptoms) {
+		Map<String, Integer> occurrences = new TreeMap<>();
+
+		for (String symptom : symptoms) {
+			occurrences.put(symptom, occurrences.getOrDefault(symptom, 0) + 1);
+		}
+
+		return occurrences;
+
+	}
+
+	public void displayOccurrences(Map<String, Integer> occurrences) {
+		for (Map.Entry<String, Integer> entry : occurrences.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+
+		writer.writeSymptoms(symptoms);
+
+	}
+
 	public static void main(String args[]) throws Exception {
-		
+
 		String filepath = "Project02Eclipse/symptoms.txt";
-		String outpath = "Project02Eclipse/result.txt";	
-		
-		
-		// Créer une instance de SymptomDataReader
-		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile(filepath);
-        List<String> symptoms = reader.GetSymptoms();
-		
-        // Créer une instance de SymptomCounter
-        SymptomCounter counter = new SymptomCounter();
-        Map<String, Integer> occurrences = counter.countOccurrences(symptoms);
-        
-        // Créer une instance de SymptomDisplay
-        SymptomDisplay display = new SymptomDisplay();
-        display.displayOccurrences(occurrences);
-        
-        // Créer une instance de WriteSymptomDataToFile
-        WriteSymptomDataToFile writer = new WriteSymptomDataToFile(outpath);
-        writer.writeSymptoms(occurrences);
-        
+		String outpath = "Project02Eclipse/result.txt";
+
+		ISymptomReader reader = new ReadSymptomDataFromFile(filepath);
+		ISymptomWriter writer = new WriteSymptomDataToFile(outpath);
+		AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+
+		List<String> symptoms = counter.getSymptoms();
+
+		Map<String, Integer> occurrences = counter.countOccurrences(symptoms);
+		counter.displayOccurrences(occurrences);
+		counter.writeSymptoms(occurrences);
+
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -5,27 +5,29 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class WriteSymptomDataToFile implements ISymptomWriter{
-	
-private String outpath;
-	
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+	private String outpath;
+
 	/**
 	 * 
-	 * @param outpath a full or partial path to file with symptom strings in it, one per line
+	 * @param outpath a full or partial path to file with symptom strings in it, one
+	 *                per line
 	 */
-	public WriteSymptomDataToFile (String outpath) {
+	public WriteSymptomDataToFile(String outpath) {
 		this.outpath = outpath;
 	}
-		
+
 	@Override
 	public void writeSymptoms(Map<String, Integer> occurrences) {
-	    try (BufferedWriter writer = new BufferedWriter(new FileWriter(outpath))) {
-	        for (Map.Entry<String, Integer> entry : occurrences.entrySet()) {
-	            writer.write(entry.getKey() + ": " + entry.getValue());
-	            writer.newLine();
-	        }writer.close();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(outpath))) {
+			for (Map.Entry<String, Integer> entry : occurrences.entrySet()) {
+				writer.write(entry.getKey() + ": " + entry.getValue());
+				writer.newLine();
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
